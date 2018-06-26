fa802d18f98215f40c9b1ee16a2c14e7d3a6ad59
commzero <wahshey.challengers@gmail.com>
2018-06-26 21:28:33 +0200

    edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,35 +11,6 @@ export class AppComponent implements OnInit {
     private getItemsService: JustCleanServiceService,
   ) { }
   title = 'Just Clean';
-  id: number;
-  name:string;
-  imageUrl: string;
-  items:any = {};
-  objects: [1,2,3,4,5];
-  increment: number = 1;
-  decrement: number = 1;  
   ngOnInit() {
-    this.getItems();
   }
-  getItems() {
-    this.getItemsService.getItems().subscribe(
-      res => {
-        console.log(res)
-        if (res) {
-          this.items = res;
-        }
-      }
-    )
-  }
-  addToCart() {
-    this.increment++;
-    console.log(this.increment);
-  }
-  removeFromCart() {
-    this.increment--;
-    if(this.increment == -1) {
-      this.increment++;
-    }
-  }
-
 }
